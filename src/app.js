9d1c961de39263a3077e8e7d1ca8a31d4fbfa727
nutbreaker <nutbreaker@users.noreply.github.com>
2020-01-17 22:43:14 +0100

    feat: log add
    * docs: add log comment
    * feat: log current date
    * docs: log current date
    * feat: log add
    * fix: merge conflict

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-(()=>{
+(() => {
     //log app status and version
     console.log('APP loaded, version', '1.4.0');
 
+    //log current date
     console.log('DATE', Date());
+
+    console.log('ADD 2 + 2', 2 + 2);
+
 })()
